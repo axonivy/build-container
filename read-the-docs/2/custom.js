e1f08c25d283c262ddfb01c00b211006a1bce0e5
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2020-07-30 13:54:15 +0200

    XIVY-4158 toggle for details in N&N

--- read-the-docs/2/custom.js
+++ read-the-docs/2/custom.js
@@ -10,3 +10,12 @@ $(document).ready(function() {
     }
   });
 });
+
+$(document).ready(function() {
+  $(".toggle > *").hide();
+  $(".toggle .header").show();
+  $(".toggle .header").click(function() {
+      $(this).parent().children().not(".header").toggle(100);
+      $(this).parent().children(".header").toggleClass("open");
+  })
+});
